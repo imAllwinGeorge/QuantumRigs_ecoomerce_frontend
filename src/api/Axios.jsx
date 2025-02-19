9e25e77695ddd8c5d1a3a0d2565bb2885ad0e9f5
Allwin P G <allwin.george19@gmail.com>
2025-02-19 22:32:49 +0530

    backend url

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
+const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 
 
 const axiosInstance = axios.create({
-    baseURL:'http://localhost:3000',
+    baseURL: backendUrl,
     withCredentials:true,
 })
 
