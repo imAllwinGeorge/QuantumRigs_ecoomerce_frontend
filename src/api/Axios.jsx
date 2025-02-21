d910a8dd602951162cb25ddff2defdbbdfc65233
Allwin P G <allwin.george19@gmail.com>
2025-02-22 03:12:45 +0530

    fix cors issue

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -6,11 +6,7 @@ console.log(backendUrl)
 const axiosInstance = axios.create({
     baseURL: backendUrl,
     withCredentials:true,
-    headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": "true",
-    },
+    
 })
 
 
